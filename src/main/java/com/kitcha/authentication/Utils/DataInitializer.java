68617af01f67b746af1b5cc1f70ac3f798b03bad
Juyoung Jeong <wdd1016@gmail.com>
2025-03-06 18:08:52 +0900

    Fix: hide test pws

--- src/main/java/com/kitcha/authentication/Utils/DataInitializer.java
+++ src/main/java/com/kitcha/authentication/Utils/DataInitializer.java
@@ -4,15 +4,24 @@ import com.kitcha.authentication.entity.UserEntity;
 import com.kitcha.authentication.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Objects;
+
 @Component
-@AllArgsConstructor
 public class DataInitializer implements CommandLineRunner {
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final String testUserPassword;
+    private final String adminPassword;
+
+    public DataInitializer(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, Environment env) {
+        this.userRepository = userRepository;
+        this.testUserPassword = bCryptPasswordEncoder.encode(Objects.requireNonNull(env.getProperty("TEST_USER_PASSWORD")));
+        this.adminPassword = bCryptPasswordEncoder.encode(Objects.requireNonNull(env.getProperty("TEST_ADMIN_PASSWORD")));
+    }
 
     @Override
     @Transactional
@@ -21,13 +30,13 @@ public class DataInitializer implements CommandLineRunner {
             UserEntity user = new UserEntity();
             user.setNickname("testUser");
             user.setEmail("test@test.com");
-            user.setPassword(bCryptPasswordEncoder.encode("1234"));
+            user.setPassword(testUserPassword);
             user.setRole("USER");
             user.setInterest("경제");
             UserEntity admin = new UserEntity();
             admin.setNickname("admin");
             admin.setEmail("admin@kitcha.shop");
-            admin.setPassword(bCryptPasswordEncoder.encode("admin"));
+            admin.setPassword(adminPassword);
             admin.setRole("ADMIN");
             userRepository.save(user);
             userRepository.save(admin);
